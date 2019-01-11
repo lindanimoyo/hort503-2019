c0246493f406f77ab92a3f1acc0feb1f71c8ec03
lmshasta <moyolindani7@gmail.com>
2019-01-11 00:10:50 -0800

    second commit

---
+++ Assignment02/ex2.py
@@ -0,0 +1,9 @@
+#A comment, this is so you can read your program later.
+#anything after the # is ignored by python
+
+print("I could have code like this") #and the comment after this is ignored
+
+#you can also use the comment to "disable" or comment out code:
+#print("This wont run")
+
+print("This will run")
---
+++ Assignment02/ex3.py
@@ -0,0 +1,41 @@
+#counting chickens
+print("I will now count my chickens:")
+
+#counting hens
+print("Hens", 25 + 30 / 6)
+
+#counting roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+#counting  eggs
+print("Now I will count the eggs:")
+
+#pemdas
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#comparison
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#comparison
+print(3 + 2 < 5 - 7)
+
+#addition
+print("What is 3 + 2?", 3 + 2)
+
+#subtraction
+print("What is 5 - 7?", 5 - 7)
+
+#exclamation
+print("Oh, that's why it's False.")
+
+#more math problems to solve
+print("How about some more.")
+
+#comparison
+print("Is it greater?", 5 > -2)
+
+#comparison
+print("Is it greater or equal?", 5 >= -2)
+
+#comparison
+print("Is it less or equal?", 5 <= -2)
